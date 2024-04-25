da486e1b91b347f55a978d5f6e3a2aa13fdca012
Freddy L <neo-ciber_94@hotmail.com>
2024-04-25 08:33:19 -0400

    remove ts from generated binary

--- packages/drizzle-project/tsup.config.ts
+++ packages/drizzle-project/tsup.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src", "!src/**/*.test.*", "!src/templates"],
-  sourcemap: true,
   clean: true,
-  dts: true,
   format: ["esm"],
 });
