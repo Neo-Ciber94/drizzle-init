31f877328f5a6889bae8a5e4ffd3235794d76ac4
Freddy L <neo-ciber_94@hotmail.com>
2024-04-25 08:43:07 -0400

    dont split code

--- packages/drizzle-project/tsup.config.ts
+++ packages/drizzle-project/tsup.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src", "!src/**/*.test.*", "!src/templates"],
   clean: true,
+  splitting: false,
   format: ["esm"],
 });
