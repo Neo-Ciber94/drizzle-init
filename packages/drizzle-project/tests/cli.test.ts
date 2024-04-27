dd88798cedacb1984f110b60fc476f270351536b
Freddy L <neo-ciber_94@hotmail.com>
2024-04-27 14:22:55 -0400

    Add more cli tests

--- packages/drizzle-project/tests/cli.test.ts
+++ packages/drizzle-project/tests/cli.test.ts
@@ -4,7 +4,7 @@ import os from "os";
 import { type InitCommandArgs } from "../src/commands/init";
 import { describe, test, expect } from "vitest";
 import { exec } from "child_process";
-import { MYSQL_DB_PROVIDERS } from "../src/types";
+import { MYSQL_DB_PROVIDERS, POSTGRESTQL_DB_PROVIDERS, SQLITE_PROVIDERS } from "../src/types";
 
 const cliPath = path.join(process.cwd(), "dist", "cli.mjs");
 
@@ -17,10 +17,14 @@ const npmInit = (cwd: string) => {
 };
 
 const npmDbGenerate = (cwd: string) => {
-  return execCommand({ cwd, cmd: "npm run db:generate", env: { DATABASE_URL: "test" } });
+  return execCommand({
+    cwd,
+    cmd: "npm run db:generate",
+    env: { DATABASE_URL: "test", DATABASE_AUTH_TOKEN: "test" },
+  });
 };
 
-describe("Run cli", () => {
+describe("Run cli with mysql driver", () => {
   for (const mysqlProvider of MYSQL_DB_PROVIDERS) {
     test(
       `Should init typescript 'mysql - ${mysqlProvider}' drizzle template`,
@@ -80,6 +84,184 @@ describe("Run cli", () => {
       }
     );
   }
+});
+
+describe("Run cli with postgresql driver", () => {
+  for (const postgresqlProvider of POSTGRESTQL_DB_PROVIDERS) {
+    test(
+      `Should init typescript 'postgres - ${postgresqlProvider}' drizzle template`,
+      { timeout: 120_000 },
+      async () => {
+        const tempDir = await createTempDir();
+
+        await npmInit(tempDir);
+
+        await runDrizzleInitProject(tempDir, {
+          driver: "postgresql",
+          dbProvider: postgresqlProvider,
+          migrateFile: "./migrate.ts",
+          configType: "typescript",
+          databaseDir: "./lib/db",
+          install: true,
+        });
+
+        await npmDbGenerate(tempDir);
+
+        await expect(fse.exists(path.join(tempDir, "./drizzle.config.ts"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./migrate.ts"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./lib/db/index.ts"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./lib/db/schema.ts"))).resolves.toBeTruthy();
+        await expect(
+          fse.exists(path.join(tempDir, "./drizzle", "meta", "_journal.json"))
+        ).resolves.toBeTruthy();
+      }
+    );
+
+    test(
+      `Should init javascript 'postgres - ${postgresqlProvider}' drizzle template`,
+      { timeout: 120_000 },
+      async () => {
+        const tempDir = await createTempDir();
+
+        await npmInit(tempDir);
+
+        await runDrizzleInitProject(tempDir, {
+          driver: "postgresql",
+          dbProvider: postgresqlProvider,
+          migrateFile: "./migrate.js",
+          configType: "javascript",
+          databaseDir: "./lib/db",
+          install: true,
+        });
+
+        await npmDbGenerate(tempDir);
+
+        await expect(fse.exists(path.join(tempDir, "./drizzle.config.js"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./migrate.js"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./lib/db/index.js"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./lib/db/schema.js"))).resolves.toBeTruthy();
+        await expect(
+          fse.exists(path.join(tempDir, "./drizzle", "meta", "_journal.json"))
+        ).resolves.toBeTruthy();
+      }
+    );
+  }
+});
+
+describe("Run cli with sqlite driver", () => {
+  for (const sqliteProvider of SQLITE_PROVIDERS) {
+    test(
+      `Should init typescript 'sqlite - ${sqliteProvider}' drizzle template`,
+      { timeout: 120_000 },
+      async () => {
+        const tempDir = await createTempDir();
+
+        await npmInit(tempDir);
+
+        await runDrizzleInitProject(tempDir, {
+          driver: "sqlite",
+          dbProvider: sqliteProvider,
+          migrateFile: "./migrate.ts",
+          configType: "typescript",
+          databaseDir: "./lib/db",
+          install: true,
+        });
+
+        await npmDbGenerate(tempDir);
+
+        await expect(fse.exists(path.join(tempDir, "./drizzle.config.ts"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./migrate.ts"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./lib/db/index.ts"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./lib/db/schema.ts"))).resolves.toBeTruthy();
+        await expect(
+          fse.exists(path.join(tempDir, "./drizzle", "meta", "_journal.json"))
+        ).resolves.toBeTruthy();
+      }
+    );
+
+    test(
+      `Should init javascript 'sqlite - ${sqliteProvider}' drizzle template`,
+      { timeout: 120_000 },
+      async () => {
+        const tempDir = await createTempDir();
+
+        await npmInit(tempDir);
+
+        await runDrizzleInitProject(tempDir, {
+          driver: "sqlite",
+          dbProvider: sqliteProvider,
+          migrateFile: "./migrate.js",
+          configType: "javascript",
+          databaseDir: "./lib/db",
+          install: true,
+        });
+
+        await npmDbGenerate(tempDir);
+
+        await expect(fse.exists(path.join(tempDir, "./drizzle.config.js"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./migrate.js"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./lib/db/index.js"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./lib/db/schema.js"))).resolves.toBeTruthy();
+        await expect(
+          fse.exists(path.join(tempDir, "./drizzle", "meta", "_journal.json"))
+        ).resolves.toBeTruthy();
+      }
+    );
+  }
+});
+
+describe("Run cli with customs options", () => {
+  test("Should generate migration on public/db folder", { timeout: 120_000 }, async () => {
+    const tempDir = await createTempDir();
+
+    await npmInit(tempDir);
+
+    await runDrizzleInitProject(tempDir, {
+      driver: "sqlite",
+      dbProvider: "libsql",
+      migrateFile: "./migrate.ts",
+      configType: "typescript",
+      databaseDir: "./lib/db",
+      outDir: "public/db",
+      install: true,
+    });
+
+    await npmDbGenerate(tempDir);
+
+    await expect(fse.exists(path.join(tempDir, "./drizzle.config.ts"))).resolves.toBeTruthy();
+    await expect(fse.exists(path.join(tempDir, "./migrate.ts"))).resolves.toBeTruthy();
+    await expect(fse.exists(path.join(tempDir, "./lib/db/index.ts"))).resolves.toBeTruthy();
+    await expect(fse.exists(path.join(tempDir, "./lib/db/schema.ts"))).resolves.toBeTruthy();
+    await expect(
+      fse.exists(path.join(tempDir, "public", "db", "meta", "_journal.json"))
+    ).resolves.toBeTruthy();
+  });
+
+  test("Should generate files with custom options", { timeout: 120_000 }, async () => {
+    const tempDir = await createTempDir();
+
+    await npmInit(tempDir);
+
+    await runDrizzleInitProject(tempDir, {
+      driver: "mysql",
+      dbProvider: "mysql2",
+      migrateFile: "/db/migrator.ts",
+      configType: "typescript",
+      databaseDir: "./db",
+      outDir: "db/migrations",
+      install: true,
+    });
+
+    await npmDbGenerate(tempDir);
+
+    await expect(fse.exists(path.join(tempDir, "./drizzle.config.ts"))).resolves.toBeTruthy();
+    await expect(fse.exists(path.join(tempDir, "./db/migrate.ts"))).resolves.toBeTruthy();
+    await expect(fse.exists(path.join(tempDir, "./db/index.ts"))).resolves.toBeTruthy();
+    await expect(fse.exists(path.join(tempDir, "./db/schema.ts"))).resolves.toBeTruthy();
+    await expect(
+      fse.exists(path.join(tempDir, "db", "migrations", "meta", "_journal.json"))
+    ).resolves.toBeTruthy();
+  });
 });
 
 type InstallOptions = { install: true } | { "no-install": true };
