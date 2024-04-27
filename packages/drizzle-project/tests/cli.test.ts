474ab9b7f6f00e529834b2d50f1ea3f39055bee2
Freddy L <neo-ciber_94@hotmail.com>
2024-04-27 01:02:19 -0400

    Add tests

--- packages/drizzle-project/tests/cli.test.ts
+++ packages/drizzle-project/tests/cli.test.ts
@@ -4,6 +4,7 @@ import os from "os";
 import { type InitCommandArgs } from "../src/commands/init";
 import { describe, test, expect } from "vitest";
 import { exec } from "child_process";
+import { MYSQL_DB_PROVIDERS } from "../src/types";
 
 const cliPath = path.join(process.cwd(), "dist", "cli.mjs");
 
@@ -12,32 +13,69 @@ if (!(await fse.exists(cliPath))) {
 }
 
 const npmInit = (cwd: string) => runCommand({ cwd, command: "npm init -y" });
-const npmDbGenerate = (cwd: string) => runCommand({ cwd, command: "npm run db:generate", env: { DATABASE_URL: "test" } });
+const npmDbGenerate = (cwd: string) =>
+  runCommand({ cwd, command: "npm run db:generate", env: { DATABASE_URL: "test" } });
 
 describe("Run cli", () => {
-  test("Should init mysql2 drizzle template", { timeout: 120_000 }, async () => {
-    const tempDir = await createTempDir();
+  for (const mysqlProvider of MYSQL_DB_PROVIDERS) {
+    test(
+      `Should init typescript 'mysql - ${mysqlProvider}' drizzle template`,
+      { timeout: 120_000 },
+      async () => {
+        const tempDir = await createTempDir();
 
-    await npmInit(tempDir);
+        await npmInit(tempDir);
 
-    await runDrizzleInitProject(tempDir, {
-      driver: "mysql",
-      dbProvider: "mysql2",
-      migrateFile: "./migrate.ts",
-      configType: "typescript",
-      databaseDir: "./lib/db",
-      install: true,
-    });
+        await runDrizzleInitProject(tempDir, {
+          driver: "mysql",
+          dbProvider: mysqlProvider,
+          migrateFile: "./migrate.ts",
+          configType: "typescript",
+          databaseDir: "./lib/db",
+          install: true,
+        });
 
-    await npmDbGenerate(tempDir);
+        await npmDbGenerate(tempDir);
 
-    await expect(fse.exists(path.join(tempDir, "./drizzle.config.ts"))).resolves.toBeTruthy();
-    await expect(fse.exists(path.join(tempDir, "./migrate.ts"))).resolves.toBeTruthy();
-    await expect(fse.exists(path.join(tempDir, "./lib/db"))).resolves.toBeTruthy();
-    await expect(
-      fse.exists(path.join(tempDir, "./drizzle", "meta", "_journal.json"))
-    ).resolves.toBeTruthy();
-  });
+        await expect(fse.exists(path.join(tempDir, "./drizzle.config.ts"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./migrate.ts"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./lib/db/index.ts"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./lib/db/schema.ts"))).resolves.toBeTruthy();
+        await expect(
+          fse.exists(path.join(tempDir, "./drizzle", "meta", "_journal.json"))
+        ).resolves.toBeTruthy();
+      }
+    );
+
+    test(
+      `Should init javascript 'mysql - ${mysqlProvider}' drizzle template`,
+      { timeout: 120_000 },
+      async () => {
+        const tempDir = await createTempDir();
+
+        await npmInit(tempDir);
+
+        await runDrizzleInitProject(tempDir, {
+          driver: "mysql",
+          dbProvider: mysqlProvider,
+          migrateFile: "./migrate.js",
+          configType: "javascript",
+          databaseDir: "./lib/db",
+          install: true,
+        });
+
+        await npmDbGenerate(tempDir);
+
+        await expect(fse.exists(path.join(tempDir, "./drizzle.config.js"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./migrate.js"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./lib/db/index.js"))).resolves.toBeTruthy();
+        await expect(fse.exists(path.join(tempDir, "./lib/db/schema.js"))).resolves.toBeTruthy();
+        await expect(
+          fse.exists(path.join(tempDir, "./drizzle", "meta", "_journal.json"))
+        ).resolves.toBeTruthy();
+      }
+    );
+  }
 });
 
 type InstallOptions = { install: true } | { "no-install": true };
@@ -87,16 +125,9 @@ async function runCommand({
   });
 }
 
-// async function createTempDir() {
-//   const tempDirName = btoa(crypto.randomUUID()).replace("=", "");
-//   const tempDir = path.join(os.tmpdir(), tempDirName);
-//   await fse.ensureDir(tempDir);
-//   console.log({ tempDir });
-//   return tempDir;
-// }
 async function createTempDir() {
-  const tempDir = path.join(os.tmpdir(), "NWQzNTVhOWEtMTU4MS00MjNjLWE2YjctNjQ3MDBjOTMxNWM1");
+  const tempDirName = btoa(crypto.randomUUID()).replace("=", "");
+  const tempDir = path.join(os.tmpdir(), tempDirName);
   await fse.ensureDir(tempDir);
-  console.log({ tempDir });
   return tempDir;
 }
